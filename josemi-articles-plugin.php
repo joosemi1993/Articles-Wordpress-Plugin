4a6e15f338a69295f8a78cb9735e7bcfe88a8aa0
Jose Miguel Calvo <josemiguelcalvo@MacBook-Pro-de-Jose.local>
2019-02-03 18:20:29 +0000

    Creating the Custom Post Type

--- josemi-articles-plugin.php
+++ josemi-articles-plugin.php
@@ -47,3 +47,55 @@ register_activation_hook( __FILE__, 'activate' );
 
 // Deactivation
 register_deactivation_hook( __FILE__, 'deactivate' );
+
+// Creating the Custom Post Type Viewer
+function art_plug_custom_post_type()  {
+	$labels = [
+		'name'                  => 'Viewer',
+        'singular_name'         => 'Viewer',
+        'add_new'               => 'Add new viewer',
+        'add_new_item'          => 'Add new viewer',
+        'edit_item'             => 'Edit viewer',
+        'view_item'             => 'View viewer',
+        'view_items'            => 'View viewers',
+        'search_items'          => 'Search viewer',
+        'not_found'             => 'Not found',
+        'not_found_in_trash'    => 'Not found in the trush',
+        'all_items'             => 'Viewers',
+        'insert_into_item'      => 'Insert',
+        'uploaded_to_this_post' => 'Uploaded',
+        'featured_image'        => 'Viewer Image',
+        'set_featured_image'    => 'set viewer image',
+        'use_featured_image'    => 'Use as viewer image',
+        'remove_featured_image' => 'Remove viewer image',	
+	];
+
+	$args = [
+        'labels'                => $labels,
+        'description'           => 'Articles with different categories',
+        'public'                => true,
+        'hierarchical'          => true,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'show_in_nav_menus'     => true,
+        'menu_position'         => 15,
+        'menu_icon'             => 'dashicons-feedback',
+        'capability_type'       => 'post',
+        'supports'              => [
+                                        'title',
+                                        'editor',
+                                        'revisions',
+                                        'thumbnail'
+                                    ],
+        'has_archive'           => true,
+        'rewrite' 				=> array( 
+								    'slug'          => 'viewer',
+								    'with_front'    => true,
+								    'hierarchical'	=> true
+								),
+        ];
+
+	register_post_type( 'viewer', $args );
+	flush_rewrite_rules();
+}
+add_action('init', 'art_plug_custom_post_type');
--- uninstall.php
+++ uninstall.php
@@ -10,3 +10,9 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	die;
 }
 
+// Clear Database stored data
+$viewers = get_posts( array( 'post_type' => 'viewer', 'numberposts' => -1 ) );
+
+foreach( $viewers as $viewer ) {
+	wp_delete_post( $viewer->ID, true );
+}
