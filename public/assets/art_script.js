6adcd6943459f6d7a7258b0eeaf460fff97a6b8c
Jose Miguel Calvo <josemiguelcalvo@MacBook-Pro-de-Jose.local>
2019-02-05 19:07:20 +0000

    Adding keyboard functionality

--- public/assets/art_script.js
+++ public/assets/art_script.js
@@ -9,16 +9,11 @@ jQuery(document).keypress(function(e) {
         // Detect the keywork
         if (lowerCharStr == "j") {
             // If j redirect to previous viewer
-            if( jQuery('.art-nav-previous a').length ) {
-			    var a_href = jQuery('.art-navigation').find('.art-nav-previous a').attr('href');
-			    window.location.href = a_href;
-			}
+            jQuery( "button.flickity-button.flickity-prev-next-button.previous" ).trigger( "click" );
         } else if (lowerCharStr == "k") {
             // If k redirect to next viewer
-            if( jQuery('.art-nav-next a').length ) {
-			    var a_href = jQuery('.art-navigation').find('.art-nav-next a').attr('href');
-			    window.location.href = a_href;
-			}
+            jQuery( "button.flickity-button.flickity-prev-next-button.next" ).trigger( "click" );
         }
     }
 });
+
